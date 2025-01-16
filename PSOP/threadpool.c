a7488faaa671f780660350eb4e62d5a97dcb1ae0
roxanageanina <andreiroxana96@yahoo.com>
2025-01-16 15:18:29 +0200

    Update threadpool.c

--- PSOP/threadpool.c
+++ PSOP/threadpool.c
@@ -5,9 +5,9 @@
 
 struct tpool_work 
 {
-    thread_func_t      func;// functia de executat
-    void              *arg;// argumentul functiei(fd)--pentru a intra in handle client
-    struct tpool_work *next;// pointer catre urmatorul task din coada
+    thread_func_t      func; // functia de executat
+    void              *arg; // argumentul functiei(fd)--pentru a intra in handle client
+    struct tpool_work *next; // pointer catre urmatorul task din coada
 };
 typedef struct tpool_work tpool_work_t;
 
@@ -18,8 +18,8 @@ struct tpool
     pthread_mutex_t  work_mutex; // pentru accesa sincronizat accesul la threaduri
     pthread_cond_t   work_cond; // conditie de a anunta task uri noi
     pthread_cond_t   working_cond; // conditie pentru a anunta terminarea task urilor
-    size_t           working_cnt;// task uri in lucru
-    size_t           thread_cnt;// thread uri in lucru
+    size_t           working_cnt; // task uri in lucru
+    size_t           thread_cnt; // thread uri in lucru
     bool             stop;
 };
 
@@ -78,7 +78,6 @@ static tpool_work_t *tpool_work_get(tpool_t *tm)
     return work;
 }
 
-// functie executata pentru fiecare thread din pool
 static void *tpool_worker(void *arg)
 {
     tpool_t      *tm = arg;
@@ -88,7 +87,6 @@ static void *tpool_worker(void *arg)
     {
         pthread_mutex_lock(&(tm->work_mutex));
 
-        // asteapta task uri sau oprirea pool ului
         while (tm->work_first == NULL && !tm->stop)
         {   
             pthread_cond_wait(&(tm->work_cond), &(tm->work_mutex));
@@ -99,12 +97,10 @@ static void *tpool_worker(void *arg)
             break;
         }
 
-        // preia task
         work = tpool_work_get(tm);
         tm->working_cnt++;
         pthread_mutex_unlock(&(tm->work_mutex));
 
-        // executa task ul
         if (work != NULL) 
         {
             work->func(work->arg);
@@ -114,7 +110,6 @@ static void *tpool_worker(void *arg)
         pthread_mutex_lock(&(tm->work_mutex));
         tm->working_cnt--;
 
-        // anunta daca toate task urile  s au terminat
         if (!tm->stop && tm->working_cnt == 0 && tm->work_first == NULL)
         {
             pthread_cond_signal(&(tm->working_cond));
@@ -135,7 +130,6 @@ tpool_t *tpool_create(size_t num)
     pthread_t  thread;
     size_t     i;
 
-    // daca numarul de thread-uri specificat (num) este zero, functia seteaza un numar implicit de 2 thread-uri
     if (num == 0)
     {    
         num = 2;
@@ -151,14 +145,13 @@ tpool_t *tpool_create(size_t num)
     tm->work_first = NULL;
     tm->work_last  = NULL;
 
-    //  fiecare thread executa functia tpool_worker, care preia si executa task-uri din coada pool-ului
     for (i=0; i<num; i++) 
     {
         pthread_create(&thread, NULL, tpool_worker, tm);
-        pthread_detach(thread); // firele sunt detasate  pentru a evita necesitatea de a le alatura (join) manual mai tarziu
+        pthread_detach(thread); 
     }
 
-    return tm; // pointerul poate fi folosit ulterior pentru a adauga task-uri, astepta finalizarea, sau distruge pool-ul 
+    return tm; 
 }
 
 
@@ -194,8 +187,6 @@ void tpool_destroy(tpool_t *tm)
     free(tm);
 }
 
-
-//adauga task nou in coada
 bool tpool_add_work(tpool_t *tm, thread_func_t func, void *arg)
 {
     tpool_work_t *work;
